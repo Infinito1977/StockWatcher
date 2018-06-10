a8dd25b559577e915010c9f3aacb360d3e861f4f
Marco Rieger <Marco.Rieger@gmx.de>
2018-06-10 15:04:31 +0200

    Removed no longer needed import

--- src/com/riegov/stockwatcher/client/Stockwatcher.java
+++ src/com/riegov/stockwatcher/client/Stockwatcher.java
@@ -13,7 +13,6 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.http.client.URL;
-import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.jsonp.client.JsonpRequestBuilder;
 import com.google.gwt.user.client.Timer;
