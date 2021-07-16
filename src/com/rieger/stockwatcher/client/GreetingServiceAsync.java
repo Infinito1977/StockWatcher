4b3be1c81102cf1f4c2d0c23610675b0582781bd
Marco Rieger <Marco.Rieger@gmx.de>
2021-07-16 04:01:27 +0200

    Initial import
    Tutorial "Build a GWT App finished"
    Next: AJAX Communication Tutorial

---
+++ src/com/rieger/stockwatcher/client/GreetingServiceAsync.java
@@ -0,0 +1,23 @@
+package com.rieger.stockwatcher.client;
+
+
+
+
+
+
+
+
+
+
+
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync
+{
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
