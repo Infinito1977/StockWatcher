40ae7a8ef6ec254cafb8bc0fe8c6281c81d4fd3a
Marco Rieger <Marco.Rieger@gmx.de>
2021-07-22 17:55:45 +0200

    Changed update interval to 1 second (from 5)

--- src/com/rieger/stockwatcher/client/StockWatcher.java
+++ src/com/rieger/stockwatcher/client/StockWatcher.java
@@ -27,7 +27,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class StockWatcher implements EntryPoint {
-	private static final int REFRESH_INTERVAL = 5000; // ms
+	private static final int REFRESH_INTERVAL = 1000; // ms
 
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private FlexTable stocksFlexTable = new FlexTable();
